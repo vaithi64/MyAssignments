6caa130cf53f5e8afb2c995f21e0ed2524e0a293
Vaithiyanthan R <82631521+vaithi64@users.noreply.github.com>
2026-04-28 20:51:23 +0530

    Add files via upload
    Day 1 assignments Uploaded

---
+++ day1datatype.js
@@ -0,0 +1,19 @@
+
+var firstName = "Vaithi"
+console.log(firstName +" "+ typeof(firstName))
+
+var companyName = "TCS"
+console.log(companyName +" "+ typeof(companyName))
+
+var mobNumber = 987654
+console.log(mobNumber +" "+ typeof(mobNumber))
+
+var isAutomation = 
+console.log(isAutomation +" "+ typeof(isAutomation))
+
+var hasPlaywright = null
+console.log(hasPlaywright+" "+ typeof(hasPlaywright))
+
+//var testingJob = true
+//console.log(testingJob+" "+ typeof(testingJob))
+
